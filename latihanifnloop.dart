ffd93e282be6fbef183a9c8d93602f182cb731f7
gradepw <xristgt@gmail.com>
2026-09-07 19:58:47 +0700

    Terbaru

--- latihanifnloop.dart
+++ latihanifnloop.dart
@@ -1,3 +1,53 @@
-void main(List<String> args) {
+// void main(List<String> args) {
+//   int kehadiran = 75;
+//   if (kehadiran < 75) {
+//     print("tidak bisa ikut ujian");
+//   } else {
+//     print("bisa ikut ujian");
+//   }
+
+//   if (kehadiran < 75) {
+//     print("tidak bisa ikut ujian");
+//   }
+
+//   if (kehadiran >= 75) {
+//     print("bisa ikut ujian");
+//   }
+
+//   var nilaiakhir = 'A';
+//   switch (nilaiakhir) {
+//     case 'A':
+//     print("nilai sangat puas");
+//     break;
+//     case 'B':
+//     print("nilai puas");
+//     break;
+//     case 'C':
+//     print("nilai cukup puas");
+//     default:
+//     print("nilai tidak dalam cakupan");
+//   }
+
+//   List datamhs = ['Andri', 'Merry'];
+//   for (var i = 0; i <datamhs.length; i++) {
+//      print(datamhs[i]);
+//   }
+
+//   for(var element in datamhs) {
+//     print(element);
+//   }
   
+//   datamhs.forEach(print);
+
+//   var index =0;
+//   while(index < 5){
+//     print(datamhs[index]);
+//     index++;
+//   }
+
+//   var index = 0;
+//   do {
+//     print(datamhs[index]);
+//     index++;
+//   } while (index < 5);
 }
